5079e05bda2a2a609314e5aaff093789f5a28c31
Kristina <kristina.maximova@edu.edu.mt>
2023-06-26 14:04:29 +0200

    Logout functionality
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/components/top_widget_profile.dart
+++ lib/components/top_widget_profile.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../login.dart';
 
 class TopWidgetProfile extends StatelessWidget {
-  const TopWidgetProfile({super.key});
+  final Map<String, dynamic>? userData;
+
+  const TopWidgetProfile({Key? key, this.userData}) : super(key: key);
+
+  Future<void> _logout(BuildContext context) async {
+    // Clear the user data from persistent storage
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('userData');
+
+    const apiBaseUrl = String.fromEnvironment('API_BASE_URL',
+        defaultValue: 'https://gamma.staging.candena.de');
+
+    // Navigate back to the login screen or any desired screen
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Login(apiBaseUrl: apiBaseUrl)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       height: 95,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
             children: [
-              Icon(
+              const Icon(
                 Icons.person_outlined,
                 size: 38,
               ),
-              SizedBox(width: 15),
-              Column(
+              const SizedBox(width: 15),
+              const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
@@ -43,6 +63,14 @@ class TopWidgetProfile extends StatelessWidget {
                       softWrap: false,
                       style: TextStyle(height: 1.50)),
                 ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              IconButton(
+                icon: const Icon(Icons.logout),
+                onPressed: () =>
+                    _logout(context), // Call the logout method on button press
               )
             ],
           ),
