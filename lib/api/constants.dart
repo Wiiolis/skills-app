b4f07bb7f3fb7c77e31737d076ed5c539f00c2da
Kristina <kristina.maximova@edu.edu.mt>
2023-07-27 13:37:24 +0200

    new endpoint
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,8 +1,13 @@
 class ApiConstants {
   static String baseUrl = 'https://gamma.staging.candena.de/api/v1/';
   static late String userEndpoint;
+  static late String clinicalSkills;
 
   static initializeUserEndpoint(int userId) {
     userEndpoint = 'users/$userId';
+  }
+
+  static initializeClinicalSkills(int moduleVersionId) {
+    clinicalSkills = 'modules/$moduleVersionId/clinical_skills';
   }
 }
