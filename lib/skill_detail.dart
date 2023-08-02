7ff01967b4a86fd585b961a2df15ce875fb7ffd3
Kristina <kristina.maximova@edu.edu.mt>
2023-08-02 16:30:45 +0200

    skill detail
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/skill_detail.dart
+++ lib/skill_detail.dart
@@ -1,4 +1,6 @@
 import 'package:demo_app/components/bottom_navigation.dart';
+import 'package:demo_app/components/button.dart';
+import 'package:demo_app/globals.dart';
 import 'package:flutter/material.dart';
 
 import 'dashboard.dart';
@@ -14,20 +16,116 @@ class _SkillDetailState extends State<SkillDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       bottomNavigationBar: const bottomNavigation(),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              BackButton(
-                  onPressed: () => Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Dashboard()),
-                      ))
-            ],
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 25, 20, 15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Dashboard()),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(Icons.chevron_left_outlined,
+                            color: AppColors.primaryColor, size: 25),
+                        Text(
+                          'Back',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: AppColors.primaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Expanded(
+                    child: Center(
+                        child: Text(
+                  'Details',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ))),
+                const Expanded(child: Text(''))
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              child: Text(
+                  'Lorem ipsum dolor sit amet and some other very very boring text that no one understands.'),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text('Role'),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Text(
+                      'Assistant M3',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text('Date of Assessing'),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Text(
+                      'Assistant M3',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text('Supervisor*'),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Text(
+                      'Assistant M3',
+                      style: TextStyle(fontWeight: FontWeight.w700),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
