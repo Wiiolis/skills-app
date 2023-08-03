924f655c6cb900bea2cd672c0892fb2863275226
Kristina <kristina.maximova@edu.edu.mt>
2023-08-03 16:17:32 +0200

    add date range to onput and format it
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/skill_detail.dart
+++ lib/skill_detail.dart
@@ -139,23 +139,24 @@ class _SkillDetailState extends State<SkillDetail> {
                         readOnly: true,
                         //set it true, so that user will not able to edit text
                         onTap: () async {
-                          DateTime? pickedDate = await showDatePicker(
+                          DateTimeRange? pickedDate = await showDateRangePicker(
                               context: context,
-                              initialDate: DateTime.now(),
-                              firstDate: DateTime(1950),
-                              //DateTime.now() - not to allow to choose before today.
-                              lastDate: DateTime(2100));
-
+                              firstDate: DateTime(2020),
+                              lastDate: DateTime(2024));
                           if (pickedDate != null) {
-                            print(
-                                pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                            String formattedDate =
-                                DateFormat('yyyy-MM-dd').format(pickedDate);
-                            print(
-                                formattedDate); //formatted date output using intl package =>  2021-03-16
+                            //pickedDate output format => 2021-03-10 00:00:00.000
+                            String startDate = DateFormat('dd/MM/yyyy')
+                                .format(pickedDate.start);
+                            String endDate =
+                                DateFormat('dd/MM/yyyy').format(pickedDate.end);
+
+                            String start = startDate.toString().split(' ')[0];
+                            String end = endDate.toString().split(' ')[0];
+
+                            //formatted date output using intl package =>  2021-03-16
                             setState(() {
-                              dateInput.text =
-                                  formattedDate; //set output date to TextField value.
+                              dateInput.text = '$start - $end';
+                              //set output date to TextField value.
                             });
                           } else {}
                         },
