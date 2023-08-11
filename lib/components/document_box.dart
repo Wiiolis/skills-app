d4e5ffe21dbbbb4cb5677738bdd78355271d9289
Kristina <kristina.maximova@edu.edu.mt>
2023-08-11 09:51:24 +0200

    use correct icon for document box
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/components/document_box.dart
+++ lib/components/document_box.dart
@@ -1,19 +1,20 @@
 import 'package:demo_app/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DocumentBox extends StatelessWidget {
   const DocumentBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      shape: RoundedRectangleBorder(
+    return Card(
+      shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10))),
       child: SizedBox(
         width: 150,
         height: 190,
         child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,17 +22,21 @@ class DocumentBox extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.file_copy_outlined,
-                      color: AppColors.primaryColor, size: 40),
-                  SizedBox(
+                  SvgPicture.asset(
+                    'assets/images/Documents.svg',
+                    color: AppColors.primaryColor,
+                    height: 36,
+                  ),
+                  const SizedBox(
                     height: 15,
                   ),
-                  Text(
+                  const Text(
                     'Absence Form',
+                    style: TextStyle(fontWeight: FontWeight.w600),
                   ),
                 ],
               ),
-              TextButton(
+              const TextButton(
                 style: ButtonStyle(),
                 onPressed: null,
                 child: Row(
