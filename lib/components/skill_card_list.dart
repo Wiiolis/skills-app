2452c20d4aeb7f8de90468d9b4373793647275fa
Kristina <kristina.maximova@edu.edu.mt>
2023-08-14 17:02:55 +0200

    New placeholder svg image and placeholder text added with the link
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- lib/components/skill_card_list.dart
+++ lib/components/skill_card_list.dart
@@ -1,9 +1,14 @@
 import 'package:demo_app/components/dropdown.dart';
 import 'package:demo_app/components/skill_card.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../api/api_service.dart';
+import '../globals.dart';
 
 class SkillCardList extends StatefulWidget {
   const SkillCardList({Key? key}) : super(key: key);
@@ -29,6 +34,11 @@ class _SkillCardListState extends State<SkillCardList> {
     });
   }
 
+  checkHospital() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('hospitalAssigned');
+  }
+
   Future<void> getselectedValueId(module) async {
     setState(() {
       selectedValue = module[0].moduleVersionId;
@@ -45,71 +55,126 @@ class _SkillCardListState extends State<SkillCardList> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(left: 5),
-          child: Text(
-            'Skills Catalogue',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+    if (checkHospital() == true) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 5),
+            child: Text(
+              'Skills Catalogue',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          FutureBuilder<dynamic>(
+            future: _modulesFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                final dropdownItems = snapshot.data ?? []; // Handle null data
+                return Dropdown(
+                  dropdownItems: dropdownItems,
+                  selectedValue:
+                      selectedValue, // Set the default selected value
+                  callback: (value) {
+                    setState(() {
+                      selectedValue =
+                          value; // Update selectedValue with the selected value
+                      _clinicalSkillsFuture =
+                          _getClinicalSkills(value); // Update the Future
+                    });
+                  },
+                  valueName: 'moduleVersionId',
+                );
+              }
+            },
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          FutureBuilder<dynamic>(
+            future: _clinicalSkillsFuture,
+            builder: (context, snapshot) {
+              final clinicalSkills = snapshot.data;
+              if (clinicalSkills == null) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return Flexible(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: clinicalSkills?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                          onTap: () {
+                            context.go("/skill-detail");
+                          },
+                          child: SkillCard(data: clinicalSkills![index]));
+                    },
+                  ),
+                );
+              }
+            },
+          ),
+        ],
+      );
+    } else {
+      return SingleChildScrollView(
+        child: Expanded(
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                'assets/images/placeholder.svg',
+              ),
+              const Text(
+                'Hello, Jane!',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              const Text('You’re not in Clinical Rotation currently.',
+                  style: TextStyle(fontSize: 16)),
+              SizedBox(
+                height: 10,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'You can stay and explore the app or ',
+                      style: const TextStyle(
+                          fontSize: 16, color: AppColors.darkGrayColor),
+                    ),
+                    TextSpan(
+                      text: 'Go to the Platform',
+                      style: const TextStyle(
+                          fontSize: 16, color: AppColors.goldColor),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          launchUrl(
+                              'https://docs.flutter.io/flutter/services/UrlLauncher-class.html'
+                                  as Uri);
+                        },
+                    ),
+                    new TextSpan(
+                      text:
+                          ' and come back when your Clinical Rotation starts!',
+                      style: const TextStyle(
+                          fontSize: 16, color: AppColors.darkGrayColor),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-        const SizedBox(
-          height: 5,
-        ),
-        FutureBuilder<dynamic>(
-          future: _modulesFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              final dropdownItems = snapshot.data ?? []; // Handle null data
-              return Dropdown(
-                dropdownItems: dropdownItems,
-                selectedValue: selectedValue, // Set the default selected value
-                callback: (value) {
-                  setState(() {
-                    selectedValue =
-                        value; // Update selectedValue with the selected value
-                    _clinicalSkillsFuture =
-                        _getClinicalSkills(value); // Update the Future
-                  });
-                },
-                valueName: 'moduleVersionId',
-              );
-            }
-          },
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        FutureBuilder<dynamic>(
-          future: _clinicalSkillsFuture,
-          builder: (context, snapshot) {
-            final clinicalSkills = snapshot.data;
-            if (clinicalSkills == null) {
-              return const Center(child: CircularProgressIndicator());
-            } else {
-              return Flexible(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: clinicalSkills?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onTap: () {
-                          context.go("/skill-detail");
-                        },
-                        child: SkillCard(data: clinicalSkills![index]));
-                  },
-                ),
-              );
-            }
-          },
-        ),
-      ],
-    );
+      );
+    }
   }
 }
